aa4f6966cad946578177970242128d297f2852f9
naciyeceren097 <naciye.ceren097@gmail.com>
2022-02-20 19:07:35 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink
+} from "react-router-dom";
+
+import React from 'react';
+import About from "./components/About";
+import Users from "./components/Users";
+import Home from "./components/Home";
+import Error404 from "./components/Error404";
+import './App.css';
+
+function App() {
+  return (
+  
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              {/* navlink bir linke tıkladığımızda o sayfadayken linkin özelleştirilmesini sağlar.. */}
+              {/* aynı zamanda activeClassName activeStyle etiketini kullanarak direkt olarak style da verebiliriz */}
+              <NavLink activeClassName="active" exact to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink activeClassName="active" to="/about">About</NavLink>
+            </li>
+            <li>
+              <NavLink activeClassName="active" to="/users">Users</NavLink>
+            </li>
+            
+          </ul>
+        </nav>
+      </div>
+
+    <Switch>
+      <Route exact path="/" component={Home}/>
+      <Route path="/about" component={About}/>      
+      <Route path="/users" component={Users}/>
+      <Route path="*" component={Error404}/>
+
+
+    </Switch>
+    </Router>
+
+  );
+}
+
+
+
+
+
+export default App;
---
+++ src/components/User.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import {useEffect,useState} from 'react';
+import {useParams, Link} from 'react-router-dom';
+
+
+function User() {
+    const [loading,setLoading] = useState(true);
+    const [user,setUser] = useState({});
+    const {id} = useParams();
+
+    useEffect(()=>{
+        axios(`https://jsonplaceholder.typicode.com/users/${id}`)
+        .then((res)=>setUser(res.data))
+        .finally(()=>setLoading(false));
+    },[id])
+  return (
+    <div>
+      <h1>User Detail</h1>
+      {loading && <div>Loading...</div>}
+     {!loading &&  <code>
+          {JSON.stringify(user)}
+      </code>}
+
+      <br></br><br></br>
+    
+
+    <Link  style={{backgroundColor: 'red',color:'white', marginRight:20}} to={`/users/${parseInt(id)-1}`}>Previous User ({parseInt(id)-1})</Link> 
+    <Link  style={{backgroundColor: 'green', color:'white'}} to={`/users/${parseInt(id)+1}`}>Next User ({parseInt(id)+1})</Link>
+
+    
+
+
+    </div>
+
+  )
+}
+
+export default User
